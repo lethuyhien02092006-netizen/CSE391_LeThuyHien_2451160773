2fb291fbc09dd6f0f56a01888d57d3a00f14f549
lethuyhien02092006-netizen <lethuyhien02092006@gmail.com>
2026-05-31 22:14:42 +0700

    PBT_07: Hoan thanh B1

--- PBT_07/calculator.js
+++ PBT_07/calculator.js
@@ -2,16 +2,13 @@ function calculate(num1, operator, num2) {
     if (typeof num1 !== "number" || typeof num2 !== "number" || isNaN(num1) || isNaN(num2)) {
         return "Lỗi: Input không phải số";
     }
-
     const validOps = ["+", "-", "*", "/", "%", "**"];
     if (!validOps.includes(operator)) {
         return "Lỗi: Operator '" + operator + "' không hợp lệ";
     }
-
     if (operator === "/" && num2 === 0) {
         return "Lỗi: Không thể chia cho 0";
     }
-
     switch (operator) {
         case "+": return num1 + num2;
         case "-": return num1 - num2;
